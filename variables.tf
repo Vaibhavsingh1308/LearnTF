a21b58f7e9e5ac5e8ab40159150194f500727481
Vaibhav-devops <singhvaibhav895@gmail.com>
2025-08-25 11:18:33 +0530

    Update variable file

--- variables.tf
+++ variables.tf
@@ -1,4 +1,20 @@
-variable "SUBSCRIPTION_ID" {}
-variable "SP_CLIENT_ID" {}
-variable "SP_CLIENT_SECRET" {}
-variable "SP_TENANT_ID" {}
+variable "SUBSCRIPTION_ID" {
+  type        = string
+  description = "Azure Subscription ID"
+}
+
+variable "SP_CLIENT_ID" {
+  type        = string
+  description = "Service Principal Client ID"
+}
+
+variable "SP_CLIENT_SECRET" {
+  type        = string
+  description = "Service Principal Client Secret"
+  sensitive   = true
+}
+
+variable "SP_TENANT_ID" {
+  type        = string
+  description = "Azure Tenant ID"
+}
