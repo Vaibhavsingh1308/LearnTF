8371ecd298499a6e35d82dfc129dfc1aaa401c21
Vaibhav-devops <singhvaibhav895@gmail.com>
2025-08-28 17:46:47 +0530

    Added templete

--- variables.tf
+++ variables.tf
@@ -1,20 +1,41 @@
 variable "SUBSCRIPTION_ID" {
-  description = "Azure subscription ID"
-  type        = string
+  description = "Azure Subscription ID"
 }
 
 variable "SP_CLIENT_ID" {
-  description = "Azure service principal client ID"
-  type        = string
+  description = "Service Principal Client ID"
 }
 
 variable "SP_CLIENT_SECRET" {
-  description = "Azure service principal client secret"
-  type        = string
+  description = "Service Principal Client Secret"
   sensitive   = true
 }
 
 variable "SP_TENANT_ID" {
-  description = "Azure tenant ID"
-  type        = string
+  description = "Azure Tenant ID"
+}
+
+variable "location" {
+  default     = "East US"
+  description = "Azure Region"
+}
+
+variable "resource_group_name" {
+  default     = "rg-tf-vm"
+  description = "Resource Group Name"
+}
+
+variable "vm_admin_username" {
+  default     = "azureuser"
+  description = "Admin username for VM"
+}
+
+variable "vm_admin_password" {
+  description = "Admin password for VM"
+  sensitive   = true
+}
+
+variable "node_docker_image" {
+  default     = "yourdockerhubuser/node-app:latest"
+  description = "Node.js Docker image"
 }
