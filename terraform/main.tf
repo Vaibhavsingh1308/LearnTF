c89038e977a6cf00d2e5ead256b29f697b983853
Vaibhav-devops <singhvaibhav895@gmail.com>
2025-09-03 10:49:52 +0530

    update terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = var.SUBSCRIPTION_ID
+  client_id       = var.SP_CLIENT_ID
+  client_secret   = var.SP_CLIENT_SECRET
+  tenant_id       = var.SP_TENANT_ID
 }
 
 # Resource Group (single RG controlled by variables)
