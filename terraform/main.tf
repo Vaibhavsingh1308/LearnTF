356dee2e52cbb451ec6e97783dd3dbbce45d7156
Vaibhav-devops <singhvaibhav895@gmail.com>
2025-09-03 11:30:57 +0530

    update asp

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,9 +27,9 @@ resource "azurerm_service_plan" "plan" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
 
-  os_type  = "Linux"
-  sku_name = "F1" # free tier
+  sku_name = "B1"  # required
 }
 
 # Linux Web App (Next.js containerized app)
