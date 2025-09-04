e770a4406ad902f0bfb33acfd5c2fab55b7c78be
Vaibhav-devops <singhvaibhav895@gmail.com>
2025-09-04 11:57:16 +0530

    Update ACR and output file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,21 @@
+# output "acr_login_server" {
+#   description = "Login server of the Azure Container Registry"
+#   value       = azurerm_container_registry.acr.login_server
+# }
+
+# output "web_app_url" {
+#   description = "URL of the Linux Web App"
+#   value       = azurerm_linux_web_app.app.default_hostname
+# }
+
 output "acr_login_server" {
   description = "Login server of the Azure Container Registry"
   value       = azurerm_container_registry.acr.login_server
+}
+
+output "acr_name" {
+  description = "Name of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.name
 }
 
 output "web_app_url" {
