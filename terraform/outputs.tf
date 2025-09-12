f697361f464af39a6d16ec0e95a1ef5002cb2549
Vaibhav-devops <singhvaibhav895@gmail.com>
2025-09-12 13:12:48 +0530

    Update output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,3 @@
-# output "acr_login_server" {
-#   description = "Login server of the Azure Container Registry"
-#   value       = azurerm_container_registry.acr.login_server
-# }
-
-# output "web_app_url" {
-#   description = "URL of the Linux Web App"
-#   value       = azurerm_linux_web_app.app.default_hostname
-# }
-
 output "acr_login_server" {
   description = "Login server of the Azure Container Registry"
   value       = azurerm_container_registry.acr.login_server
